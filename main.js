4159c70e81708091257c6f9f7d360d6797eaef7d
Shelby Paxton <shelby.paxton4@gmail.com>
2023-01-22 04:16:23 +0000

    testing

--- main.js
+++ main.js
@@ -1,38 +1,18 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.action.setBadgeText({
-      text: "ON",
-    });
-  });
-  
-  const youtube = 'https://www.youtube.com/'
+const link = document.querySelector("a");
+const image = document.querySelector("img");
 
-  
-  // When the user clicks on the extension action
-  chrome.action.onClicked.addListener(async (tab) => {
-    if (tab.url.startsWith(youtube)) {
-      // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
-      const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-      // Next state will always be the opposite
-      const nextState = prevState === 'ON' ? 'OFF' : 'ON'
-  
-      // Set the action badge to the next state
-      await chrome.action.setBadgeText({
-        tabId: tab.id,
-        text: nextState,
-      });
-  
-      if (nextState === "ON") {
-        // Insert the CSS file when the user turns the extension on
-        await chrome.scripting.insertCSS({
-          files: ["ponder.css"],
-          target: { tabId: tab.id },
-        });
-      } else if (nextState === "OFF") {
-        // Remove the CSS file when the user turns the extension off
-        await chrome.scripting.removeCSS({
-          files: ["ponder.css"],
-          target: { tabId: tab.id },
-        });
-      }
-    }
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (link) {
+  await chrome.scripting.insertCSS({
+    files: ["ponder.css"],
+    target: { tabId: tab.id },
   });
+}
+
+if (image) {
+  await chrome.scripting.insertCSS({
+    files: ["ponder.css"],
+    target: { tabId: tab.id },
+  });
+}
+      
