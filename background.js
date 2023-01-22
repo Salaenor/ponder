fd2ec6c8c3178fc58f08d12eb4bfe917fa66be93
Shelby Paxton <shelby.paxton4@gmail.com>
2023-01-22 03:47:26 +0000

    testing

--- background.js
+++ background.js
@@ -4,11 +4,13 @@ chrome.runtime.onInstalled.addListener(() => {
     });
   });
   
-  const youtube = 'https://developer.chrome.com/docs/extensions'
+  const extensions = 'https://developer.chrome.com/docs/extensions'
+const webstore = 'https://developer.chrome.com/docs/webstore'
+  const youtube = 'https://www.youtube.com/'
   
   // When the user clicks on the extension action
   chrome.action.onClicked.addListener(async (tab) => {
-    if (tab.url.startsWith(youtube)) {
+    if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore) || tab.url.startsWith(youtube)) {
       // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
       const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
       // Next state will always be the opposite
